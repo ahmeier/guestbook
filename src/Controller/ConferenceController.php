17d99d4fa4ac047ba40693f865a0a04e7ec2cb93
Andreas H Meier <ameier@ahmeier.org>
2020-07-04 18:03:20 -0400

    added spam checker

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use App\Utils\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -18,6 +19,7 @@ use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 use Exception;
+use RuntimeException;
 
 class ConferenceController extends AbstractController
 {
@@ -65,6 +67,7 @@ class ConferenceController extends AbstractController
      * @param Request $request
      * @param Conference $conference
      * @param CommentRepository $commentRepository
+     * @param SpamChecker $spamChecker
      * @param string $photoDir
      * @return Response
      * @throws LoaderError
@@ -76,6 +79,7 @@ class ConferenceController extends AbstractController
         Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
+        SpamChecker $spamChecker,
         string $photoDir
     ): Response
     {
@@ -96,6 +100,18 @@ class ConferenceController extends AbstractController
             }
 
             $this->entityManager->persist($comment);
+
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri()
+            ];
+
+            if ($spamChecker->getSpamScore($comment, $context) > 0) {
+                throw new RuntimeException('this is likely spam, go away!');
+            }
+
             $this->entityManager->flush();
 
             return $this->redirectToRoute('conference_show', ['slug' => $conference->getSlug()]);
---
+++ src/Utils/SpamChecker.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Utils;
+
+use App\Entity\Comment;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use DateTimeImmutable;
+use RuntimeException;
+
+class SpamChecker
+{
+    /**
+     * @var HttpClientInterface
+     */
+    private HttpClientInterface $client;
+    private string $endpoint;
+
+    /**
+     * SpamChecker constructor.
+     * @param HttpClientInterface $client
+     * @param string $akismetKey
+     */
+    public function __construct(HttpClientInterface $client, string $akismetKey)
+    {
+        $this->client = $client;
+        $this->endpoint = sprintf('https://%s.rest.akismet.com/1.1/comment-check', $akismetKey);
+    }
+
+    /**
+     * @param Comment $comment
+     * @param array $context
+     * @return int
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     */
+    public function getSpamScore(Comment $comment, array $context): int
+    {
+        /** @var DateTimeImmutable $commentDate */
+        $commentDate = $comment->getCreatedAt();
+        $response = $this->client->request('POST', $this->endpoint, [
+           'body' => array_merge($context, [
+               'blog' => 'https://guestbook.example.com',
+               'comment_type' => 'comment',
+               'comment_author' => $comment->getAuthor(),
+               'comment_author_email' => $comment->getEmail(),
+               'comment_content' => $comment->getText(),
+               'comment_date_gmt' => $commentDate->format('c'),
+               'blog_lang' => 'en',
+               'blog_charset' => 'UTF-8',
+               'is_test' => true
+           ])
+        ]);
+
+        $headers = $response->getHeaders();
+
+        if ('discard' === ($headers['x-akismet-pro-tip'][0] ?? '')) {
+            return 2;
+        }
+
+        $content = $response->getContent();
+        if (isset($headers['x-akismet-debug-help'][0])) {
+            throw new RuntimeException(
+                'Unable to check for spam: %s (%s)', $content, $headers['x-akismet-debug-help'][0]
+            );
+        }
+
+        return true === $content ? 0 : 1;
+    }
+}
